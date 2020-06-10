84accc2c3a85f8054203a0366c36371b03a4e54f
metap <metaperspective@gmail.com>
2020-06-09 20:34:57 -0700

    started processor.py

--- reminders/processor.py
+++ reminders/processor.py
@@ -1,12 +1,29 @@
 # Takes CSV as input
 # Creates pandas object -> JSON
-
+from pprint import pprint
 import pandas as pd
+import datetime
+import time
 
 
-def run():
+def get_reminders_from_csv():
+    """
+    takes a CSV as input and returns a remindersJSON object
+    :return: JSON representation of the input csv
+    """
     reminders = pd.read_csv('reminders.csv', header=None)
-    print(reminders)
+    reminders = reminders.rename({0: 'reminder'}, axis=1)
+    remindersJSON = reminders.to_json(orient='records')
+    return remindersJSON
+
+
+def add_datestamp():
+    """
+    takes a remindersJSON object and injects the current
+    time and date
+    :return: reminderJSON
+    """
+
 
 if __name__ == "__main__":
     run()
